ba7cc1d6ba8c9a5364a33f213a336c59c43e3e27
ethant <ethant@synnex.com>
2020-06-16 17:58:31 +0800

    upload file

--- restfulwebservice_2/src/main/java/com/ethant/buildwithmaven_3/HelloWorld.java
+++ restfulwebservice_2/src/main/java/com/ethant/buildwithmaven_3/HelloWorld.java
@@ -1,11 +1,13 @@
 package com.ethant.buildwithmaven_3;
 
+
 import org.joda.time.LocalTime;
 
 public class HelloWorld {
     public static void main(String[] args) {
         LocalTime currentTime =new LocalTime();
         System.out.println("The current local time is:" +currentTime);
-
+//        Greeter greeter =new Greeyer();
+//        System.out.println(greeter.sayHello());
     }
 }
---
+++ restfulwebservice_2/src/test/java/com/ethant/buildwithmaven_3/GreeterTest.java
@@ -0,0 +1,11 @@
+package com.ethant.buildwithmaven_3;
+
+
+import org.junit.Test;
+
+public class GreeterTest {
+    @Test
+    public void greeterSaysHello(){
+        System.out.println("什么鬼东西");
+    }
+}
